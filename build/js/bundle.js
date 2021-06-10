b64636cfd14b2f0d2ec6567be34f12e6839e0d54
Alejo29 <diegoleandro-29@hotmail.com>
2021-06-10 13:32:53 -0500

    Ready

---
+++ build/js/bundle.js
@@ -0,0 +1,2 @@
+var app=document.getElementById("app"),typewriter=new Typewriter(app,{loop:!0});typewriter.typeString("Will Royero").pauseFor(2500).deleteAll().start();
+//# sourceMappingURL=bundle.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,63 @@
 const {series, src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
+//Utilidades
+
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+
 
 //Compilacion del SASS
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*'
+}
+
 function scss(){
-    return src('src/scss/app.scss')
+    return src(paths.scss)
+    .pipe(sourcemaps.init())
     .pipe( sass())
+    .pipe( postcss( [autoprefixer(), cssnano()]))
+    .pipe( sourcemaps.write('.'))
     .pipe( dest('./build/css'));
 }
 
+function javascript(){
+    return src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe( concat('bundle.js'))
+    .pipe( terser())
+    .pipe( sourcemaps.write('.'))
+    .pipe( rename({ suffix: '.min'}))
+    .pipe( dest('./build/js'))
+}
+
+function imagenesWebp(){
+    return src(paths.imagenes)
+    .pipe( webp())
+    .pipe( dest('./build/img'))
+}
+
+function imagenes(){
+    return src(paths.imagenes)
+    .pipe( imagemin())
+    .pipe( dest('./build/img'))
+}
+
 function watchArchivos(){
-    watch('src/scss/**/*.scss', scss);
+    watch(paths.scss, scss);
 }
 
 
 
-exports.default = series(scss, watchArchivos);
+exports.default = series(scss, imagenes, imagenesWebp, javascript, watchArchivos);
